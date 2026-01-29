cdce4f151121d8abf6ee6dcee29ad8498c36a6a0
airooc41n <ai.rooc41n@gmail.com>
2026-01-29 20:54:30 +0100

    Create sw.js

---
+++ pwa/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'vino-cache-v1';
+const assets = [
+  'index.html',
+  'manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+  'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
